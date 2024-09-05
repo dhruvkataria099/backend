17f3dba710ae0b4360d5e5d95262dd203ef9e4d9
princeitaliya <pditaliya2004@gmail.com>
2024-09-05 07:58:28 +0530

    update in server file

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import express from 'express'
+import cors from 'cors'
 
 const server = express()
 
+server.use(express.json())
+server.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
 server.get('/',(req,res) => {
-    console.log(req);
-    
-    res.send(`YOU REQUEST IS RESPONSE TO YOU ${req.query.pref_productdivName}`)
+    console.log('we get your request and responsing your response');
+    res.send({message: "we get your request and responsing your response"})
 })
+
 server.get('/api/about',(req,res) => {
-    console.log('about page');
+    console.log('we get your request and responsing your response');
+    res.send({message: "we get your request and responsing your response"})
 })
 
 server.listen(8080, () => {
