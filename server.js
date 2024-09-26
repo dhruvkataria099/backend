ce367564f05c2c3b073dbce2a2497894ba6a0f65
dhruvkataria099 <dhruvkataria099@gmail.com>
2024-09-26 08:10:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://backend-tbts.onrender.com',
     methods: ['GET','POST','PUT','DELETE'],
     credentials: true
 }))
